e87b08dd2247c5455b6f109515029c44123c7976
RavicharanN <ravicharan.vsp@gmail.com>
2017-06-09 12:26:51 +0530

    initialize project

---
+++ source/profileportal/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#admin username: softdev
+#admin password: softdev123
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
