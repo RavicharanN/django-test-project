5fcc9b61e33a074672ee75af9c5e63665f0e03ad
RavicharanN <ravicharan.vsp@gmail.com>
2017-06-10 02:33:54 +0530

    Create modelsand migrate to db

--- source/profileportal/admin.py
+++ source/profileportal/admin.py
@@ -4,5 +4,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import StudentSite
 
 # Register your models here.
+class StudentSiteAdmin(admin.ModelAdmin):
+	list_display = ["__unicode__","timestamp","updated_on"]
+	class Meta:
+		model = StudentSite
+
+admin.site.register(StudentSite,StudentSiteAdmin)
