f8d2dc765ae2fc730d2afede081f5bac3edd15c8
RavicharanN <ravicharan.vsp@gmail.com>
2017-06-10 16:22:29 +0530

    Create form.py for validation

--- source/profileportal/admin.py
+++ source/profileportal/admin.py
@@ -5,11 +5,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import StudentSite
+from .forms import StudentSiteForm
 
 # Register your models here.
 class StudentSiteAdmin(admin.ModelAdmin):
 	list_display = ["__unicode__","timestamp","updated_on"]
-	class Meta:
-		model = StudentSite
+	form = StudentSiteForm
+	#class Meta:
+	#	model = StudentSite
 
 admin.site.register(StudentSite,StudentSiteAdmin)
